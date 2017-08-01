539c593bccc36f2ba988e56d090c0122799a66be
Bakytzhan Seitkazin <been.beezy@gmail.com>
2017-08-02 00:52:07 +0600

    tutorial point 16

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /blog/ 
     url(r'^$', views.index, name='index'),
+    # /blog/{id}
+    url(r'^(?P<blog_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
 def index(request):
-	return HttpResponse('<h1> Hello World, Blog Page </h1>')
+	all_albums = Album.objects.all()
+	return render(request, 'blog/index.html', {'all_albums' : all_albums})
+
+def detail(request, blog_id):
+	try:
+		album = Album.objects.get(pk = blog_id)
+	except Album.DoesNotExist:
+		raise Http404("Album does not exist")
+	return render(request, 'blog/detail.html', {'album' : album})
