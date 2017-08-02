c5fcf88baa1b8583365a4e4f045297e5c810d7ce
Bakytzhan Seitkazin <base@ecc.kz>
2017-08-02 18:23:12 +0600

    tutorial 24

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
 	# /blog/ 
     url(r'^$', views.index, name='index'),
-    # /blog/{id}
+    # /blog/{blog_id}
     url(r'^(?P<blog_id>[0-9]+)/$', views.detail, name='detail'),
+    # /blog/{blog_id}/favorite/
+    url(r'^(?P<blog_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 # Create your views here.
 def index(request):
@@ -8,8 +7,19 @@ def index(request):
 	return render(request, 'blog/index.html', {'all_albums' : all_albums})
 
 def detail(request, blog_id):
-	try:
-		album = Album.objects.get(pk = blog_id)
-	except Album.DoesNotExist:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk = blog_id)
 	return render(request, 'blog/detail.html', {'album' : album})
+
+def favorite(request, blog_id):
+	album = get_object_or_404(Album, pk = blog_id)
+	try:
+		selected_song = album.song_set.get(pk = request.POST['song'])
+	except (KeyError, Song.DoesNotExist):
+		return render(request, 'blog/detail.html', {
+			'album' : album,
+			'error_message' : "You did not select a valid song",
+			})
+	else:
+		selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'blog/detail.html', {'album' : album})
